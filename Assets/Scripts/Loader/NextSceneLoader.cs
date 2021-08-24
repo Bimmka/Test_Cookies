251815122890b43d5ff9b079af3e4d9bcf278bb5
Bimmka <bibimmka@gmail.com>
2021-08-24 12:16:30 +0400

    Loader button

---
+++ Assets/Scripts/Loader/NextSceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NextSceneLoader : MonoBehaviour
+{
+    [SerializeField] private Button loadButton;
+
+    private void Awake()
+    {
+        loadButton.onClick.AddListener(LoadArkanoidLevel);
+    }
+
+    private void OnDestroy()
+    {
+        loadButton.onClick.RemoveListener(LoadArkanoidLevel);
+    }
+
+    private void LoadArkanoidLevel()
+    {
+        SceneManager.LoadScene("Arkanoid");
+    }
+}
